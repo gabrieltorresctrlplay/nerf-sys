5c61708a9b1f0e05dd90e8ad7a0179183c608570
Gabriel Rodrigues Torres <gabriel.torres.ctrlplay@gmail.com>
2025-12-02 15:54:07 -0300

    teste 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./{components,contexts,lib}/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx"
+  ],
   darkMode: 'class',
   theme: {
     extend: {
