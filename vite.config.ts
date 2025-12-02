23a9661725edb0129265a20fee80637dddb3fe3b
Gabriel Rodrigues Torres <gabriel.torres.ctrlplay@gmail.com>
2025-12-02 16:05:52 -0300

    teste 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,17 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/nerf-sys/',
-      build: {
-        outDir: 'docs',
-      },
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/nerf-sys/',
+  plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
 });
